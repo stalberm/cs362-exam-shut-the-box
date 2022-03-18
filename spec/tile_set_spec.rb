6a2cc46def3b683305242eb80f2eebdfc4ccf5f5
Micah Stalberg <stalberm@oregonstate.edu>
2022-03-17 20:17:06 -0700

    Add unit test for TileSet ::generate method when passed specific number

--- spec/tile_set_spec.rb
+++ spec/tile_set_spec.rb
@@ -10,7 +10,9 @@ describe 'A tile set' do
     end
 
     it 'returns an array of tiles of a specified length' do
-      skip
+      specific_length = 5
+      tiles = TileSet.generate(specific_length)
+      expect(tiles.length()).to eq specific_length
     end
 
     it 'returns an array of tiles whose values increase from 1, by 1' do
